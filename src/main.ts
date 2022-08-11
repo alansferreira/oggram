31d4bb0098adc9e161885e80ef7db898f46ef59d
Alan S. Ferreira <alansferreira1984@gmail.com>
2022-08-10 22:12:36 -0300

    fix: dev config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { setup } from './server'
+import { yellow } from 'colors'
+import { packageJson } from './modules/manifest'
 
 const port = parseInt(process.env.PORT || '3000')
-
 setup().then((app) => {
   app.listen(port, () => {
-    console.log(`Server listen on port {${port}}🚀!`)
+    console.log(
+      `🚀 ${packageJson.name}: Server listen on port ${yellow(`${port}`)}!`
+    )
   })
 })
