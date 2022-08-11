af65836c7c863f034664307026a493f6b12e11a0
Alan S. Ferreira <alansferreira1984@gmail.com>
2022-08-11 10:59:57 -0300

    chore: add connection stats logs

--- src/modules/db/service.ts
+++ src/modules/db/service.ts
@@ -1,6 +1,20 @@
 import { MongoClient } from 'mongodb'
+import { red } from 'colors'
 
 const uri = 'mongodb://127.0.0.1:27017'
 
-export const client = new MongoClient(uri)
+export const client = new MongoClient(uri, {
+  connectTimeoutMS: 5000,
+  socketTimeoutMS: 5000
+})
 export const db = client.db('gram', { ignoreUndefined: true })
+
+client.connect((error) => {
+  if (error) {
+    console.error(`Database connection error from ${red(uri)}`)
+    console.error()
+    console.error(error)
+    process.exit(1)
+  }
+  console.log(`Database connection success!`)
+})
