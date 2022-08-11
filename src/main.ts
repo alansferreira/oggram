9f0226e672a30fdc4f3a30d32a3ca59ee040a0b9
Alan S. Ferreira <alansferreira1984@gmail.com>
2022-08-11 11:38:09 -0300

    feat: dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { setup } from './server'
-import { yellow } from 'colors'
+import { red, yellow } from 'colors'
 import { packageJson } from './modules/manifest'
+import { client } from './modules/db'
+import { config } from 'dotenv'
+
+config({ override: false })
+
+const { MONGODB_URI } = process.env
 
 const port = parseInt(process.env.PORT || '3000')
-setup().then((app) => {
+setup().then(async (app) => {
+  try {
+    await client.connect()
+    console.log(`Database connection success!`)
+  } catch (error) {
+    if (error) {
+      console.error(`Database connection error from ${red(MONGODB_URI)}`)
+      console.error()
+      console.error(error)
+      process.exit(1)
+    }
+  }
+
   app.listen(port, () => {
     console.log(
       `🚀 ${packageJson.name}: Server listen on port ${yellow(`${port}`)}!`
--- src/modules/db/service.ts
+++ src/modules/db/service.ts
@@ -1,20 +1,12 @@
 import { MongoClient } from 'mongodb'
-import { red } from 'colors'
+import { config } from 'dotenv'
 
-const uri = 'mongodb://127.0.0.1:27017'
+config({ override: false })
 
-export const client = new MongoClient(uri, {
+const { MONGODB_URI } = process.env
+
+export const client = new MongoClient(MONGODB_URI, {
   connectTimeoutMS: 5000,
   socketTimeoutMS: 5000
 })
-export const db = client.db('gram', { ignoreUndefined: true })
-
-client.connect((error) => {
-  if (error) {
-    console.error(`Database connection error from ${red(uri)}`)
-    console.error()
-    console.error(error)
-    process.exit(1)
-  }
-  console.log(`Database connection success!`)
-})
+export const db = client.db('oggram', { ignoreUndefined: true })
