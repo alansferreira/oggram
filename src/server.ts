50b75b380cc1f2047cfcb5ebd76b4cd8da076156
Alan S. Ferreira <alansferreira1984@gmail.com>
2022-08-12 08:21:55 -0300

    chore: cors allow any origins for tests

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import mainRoutes from './routes'
+import cors from 'cors'
 export const setup = async () => {
   const app = express()
   app.use(express.json())
   app.use(mainRoutes)
-
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        return callback(null, true)
+      }
+    })
+  )
   return app
 }
