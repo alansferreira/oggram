a01d54f27231ac62e14da4aadb9e7d85d6a561a7
Alan S. Ferreira <alansferreira1984@gmail.com>
2022-08-12 12:30:39 -0300

    test: remove unecessary jest config

--- .jest/jest-default.js
+++ .jest/jest-default.js
@@ -7,35 +7,5 @@ module.exports = {
   testRegex: '.spec\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   modulePathIgnorePatterns: ['src/mocks'],
-  coveragePathIgnorePatterns: ['src/mocks'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-    '!src/**/main.ts',
-    '!src/**/index.ts'
-  ],
-  coverageReporters: ['json', 'text', 'lcov', 'clover', 'cobertura'],
-  reporters: [
-    'default',
-    [
-      './node_modules/jest-html-reporter/dist',
-      {
-        pageTitle: 'Test Report',
-        outputPath: '.reports/test-report.html',
-      },
-    ],
-    [
-      'jest-junit',
-      {
-        suiteName: 'jest tests',
-        outputDirectory: '.reports',
-        outputName: 'test-report.xml',
-        uniqueOutputName: 'false',
-        classNameTemplate: '{classname}-{title}',
-        titleTemplate: '{classname}-{title}',
-        ancestorSeparator: ' > ',
-        usePathForSuiteName: 'true',
-      },
-    ],
-  ]
+  coveragePathIgnorePatterns: ['src/mocks']
 }
